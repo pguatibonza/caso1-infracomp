028832c7ce41e09e5b55e4d7f9891fd1b40343ef
pguatibonza <p.guatibonza@uniandes.edu.co>
2022-08-24 15:41:10 -0500

    procesos y buzones instanciados

--- caso1/src/App.java
+++ caso1/src/App.java
@@ -5,9 +5,37 @@ public class App {
         Scanner scanner= new Scanner(System.in);
         System.out.println("Ingrese el tamaño del buzon de inicio y fin: ");
         int tamanioBuzonGrande=scanner.nextInt();
-        
         System.out.println("Ingrese el tamaño del buzon intermedio: ");
         int tamanioBuzon=scanner.nextInt();
+
+        //Creacion de los buzones
+        Buzon buzonInicio= new Buzon(tamanioBuzonGrande);
+        Buzon buzonFinal= new Buzon(tamanioBuzonGrande);
+        Buzon[][] buzonesIntermedios=new Buzon[2][3];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                buzonesIntermedios[i][j]=new Buzon(tamanioBuzon);
+            }
+        }
+        //Creacion de los procesos
+        ProcesoInicial procesoInicial= new ProcesoInicial(buzonInicio);
+        ProcesoFinal procesoFinal=new ProcesoFinal(buzonFinal);
+        ProcesoIntermedio[][] procesosIntermedios=new ProcesoIntermedio[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if (j==0){
+                    procesosIntermedios[i][j]=new ProcesoIntermedio(buzonInicio, buzonesIntermedios[i][j],i,j);
+                }
+                else if (j==1){
+                    procesosIntermedios[i][j]=new ProcesoIntermedio(buzonesIntermedios[i][j-1], buzonesIntermedios[i][j],i,j);
+                }
+                else{
+                    procesosIntermedios[i][j]=new ProcesoIntermedio(buzonesIntermedios[i][j-1], buzonFinal,i,j);
+                }
+            }
+        }
+        //Inicio de los procesos
+        
 
 
 
